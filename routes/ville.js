e4583d7ef077cd37f51dd15fc5e0d808655141bd
tkaing <t.kaing13@gmail.com>
2019-03-13 11:59:42 +0100

    method POST

--- routes/ville.js
+++ routes/ville.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	var ville = req.param('nom_ville');
+router.post('/', function(req, res, next) {
+	var ville = req.body.nom_ville;
 	res.render('ville', { title: 'Node App', ville: ville });
 });
 
